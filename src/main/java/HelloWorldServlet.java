6b1530de1b53913fc0509838c60706c636acbc2c
Michael Baker <michael.a.baker34@gmail.com>
2021-04-08 09:27:53 -0500

    complete hello variable exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,22 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
 
+//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        String thingToGreet = req.getParameter("name");
+        if (thingToGreet == null) {
+            thingToGreet = "world";
+        }
+        String output = String.format("<h1>Hello, %s</h1>", thingToGreet);
+        out.println(output);
     }
+
 }
