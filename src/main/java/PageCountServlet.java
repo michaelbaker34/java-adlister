0654661bc36fd3b4f36e7679705115734940ad88
Michael Baker <michael.a.baker34@gmail.com>
2021-04-08 09:38:20 -0500

    complete page count exercise

---
+++ src/main/java/PageCountServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/count")
+public class PageCountServlet extends HttpServlet {
+
+    int pageCountViews = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+        pageCountViews += 1;
+        System.out.println(pageCountViews);
+    }
+
+}
